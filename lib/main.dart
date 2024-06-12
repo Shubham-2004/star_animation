dbf82266c9a3a737f71226961f88117ea3dcad35
Shubham-2004 <shubhambera100@gmail.com>
2024-06-12 17:25:31 +0530

    final_color_animation change

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:icon_animation/provider/star_animation_provider.dart';
-import 'package:icon_animation/star_animation_screens/star_animation1.dart';
-import 'package:icon_animation/star_animation_screens/star_animation2.dart';
 import 'package:icon_animation/star_animation_screens/star_animation3.dart';
-import 'package:icon_animation/star_animation_screens/star_animation4.dart';
-import 'package:icon_animation/star_animation_screens/star_animation5.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -27,11 +23,7 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              StarAnimation1(),
-              StarAnimation2(),
               StarAnimation3(),
-              StarAnimation4(),
-              StarAnimation5(),
             ],
           ),
         ),
--- lib/provider/star_animation_provider.dart
+++ lib/provider/star_animation_provider.dart
@@ -1,38 +1,11 @@
 import 'package:flutter/material.dart';
 
 class StarAnimationProvider with ChangeNotifier {
-  bool _isClicked1 = false;
-  bool get isClicked1 => _isClicked1;
-  bool _isClicked2 = false;
-  bool get isClicked2 => _isClicked2;
   bool _isClicked3 = false;
   bool get isClicked3 => _isClicked3;
-  bool _isClicked4 = false;
-  bool get isClicked4 => _isClicked4;
-  bool _isClicked5 = false;
-  bool get isClicked5 => _isClicked5;
-  void toggleClicked1() {
-    _isClicked1 = !_isClicked1;
-    notifyListeners();
-  }
-
-  void toggleClicked2() {
-    _isClicked2 = !_isClicked2;
-    notifyListeners();
-  }
 
   void toggleClicked3() {
     _isClicked3 = !_isClicked3;
-    notifyListeners();
-  }
-
-  void toggleClicked4() {
-    _isClicked4 = !_isClicked4;
-    notifyListeners();
-  }
-
-  void toggleClicked5() {
-    _isClicked5 = !_isClicked5;
     notifyListeners();
   }
 }
